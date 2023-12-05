acf32e8615a93c176273f460b07a024c5b572578
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-12-05 15:25:37 -0500

    finsihed standard error, fixed count function, changed out functions that were not permissable

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+from statzcw import zcount
 from statzcw import zvariance
 from statzcw import zstddev
 from statzcw import zmode
 from statzcw import zmedian
+from statzcw import zstderr
+from statzcw import zcorr
+
+print(zcount([2, 4, 5, 456]))
 
 print(zvariance([46, 69, 32, 60, 52, 41]))
 
@@ -10,3 +15,7 @@ print(zstddev([9, 24, 31]))
 print(zmode([1, 2, 3, 7, 7, 7, 4, 4, 5, 6,  8, 8]))
 
 print(zmedian([12, 34, 54, 45, 34]))
+
+print(zstderr([67, 78, 89, 90]))
+
+print(zcorr([1, 2, 3], [10, 20, 27]))
--- statzcw.py
+++ statzcw.py
@@ -1,15 +1,21 @@
 import math
 
-zcount = lambda lis: sum(lis)
+def zcount(lis: list) -> float:
+    count = 0
+    for l in lis:
+        count += 1
+    return count
 
-zmean = lambda lis: zcount(lis) / len(lis)
+zmean = lambda lis: sum(lis) / zcount(lis)
 
 def zmode(lis: list) -> float:
     max_count = 0
     max_freq = 0
-    for ls in set(lis):
+    # making a new list as a set to be iterated through
+    new_lis = [x for x in lis if x == x]
+    for ls in new_lis:
         counter = 0
-        for l in sorted(lis):
+        for l in lis:
             if l == ls:
                 counter += 1
         if counter > max_count:
@@ -18,13 +24,18 @@ def zmode(lis: list) -> float:
     return max_freq
 
 
-zmedian = lambda lis: lis[len(lis) // 2]
+zmedian = lambda lis: lis[zcount(lis) // 2]
+
 
 def zvariance(lis: list) -> float:
     new_lis1 = [x - zmean(lis) for x in lis]
     new_lis2 = [x*x for x in new_lis1]
-    return zcount(new_lis2) / (len(lis) - 1)
+    return sum(new_lis2) / (zcount(lis) - 1)
 
 
 zstddev = lambda lis: math.sqrt(zvariance(lis))
+
+zstderr = lambda lis: math.sqrt(zstddev(lis) * zstddev(lis)) / math.sqrt(zcount(lis))
+
+zcorr = lambda listx, listy: (zcount(listx) / zmean(listx)) / (zcount(listy) / zmean(listy))
 

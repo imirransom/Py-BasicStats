2bdab66b77aa76a7ebfdeb908c46240ceae7a0d6
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-12-04 21:34:24 -0500

    got standard deviation and mode working

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from statzcw import zvariance
+from statzcw import zstddev
+from statzcw import zmode
+from statzcw import zmedian
 
 print(zvariance([46, 69, 32, 60, 52, 41]))
+
+print(zstddev([9, 24, 31]))
+
+print(zmode([1, 2, 3, 7, 7, 7, 4, 4, 5, 6,  8, 8]))
+
+print(zmedian([12, 34, 54, 45, 34]))
--- statzcw.py
+++ statzcw.py
@@ -5,10 +5,20 @@ zcount = lambda lis: sum(lis)
 zmean = lambda lis: zcount(lis) / len(lis)
 
 def zmode(lis: list) -> float:
-    pass
+    max_count = 0
+    max_freq = 0
+    for ls in set(lis):
+        counter = 0
+        for l in sorted(lis):
+            if l == ls:
+                counter += 1
+        if counter > max_count:
+            max_count = counter
+            max_freq = ls
+    return max_freq
 
 
-zmedian = lambda lis: len(lis) // 2
+zmedian = lambda lis: lis[len(lis) // 2]
 
 def zvariance(lis: list) -> float:
     new_lis1 = [x - zmean(lis) for x in lis]
